b1fdce6c3eda6686a13810b3216e5b1c1ea676d4
Chris15797 <geyer.c@web.de>
2021-02-21 23:01:30 +0100

    Request-Server

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 
 import socketserver
 
-from tcp_server import TCPSocketHandler
+from tcp_server import CommunicationHandler
+from tcp_server import RequestHandler
 
 if __name__ == "__main__":
 # set Host and Port
     HOST, PORT = "localhost", 10000
+    HOSTR, PORTR = "localhost", 10001
 #instantiate Server and Bynd to Port and Handler
-    server = socketserver.TCPServer(('',PORT),TCPSocketHandler)
-    print("Opened Server")
+    servercom = socketserver.TCPServer(('',PORT),CommunicationHandler)
+    serverreq = socketserver.TCPServer(('',PORTR),RequestHandler)
+    print("Opened Servers")
 #tell the Server to run until program end
-    server.serve_forever()
+    servercom.serve_forever()
+    serverreq.serve_forever()
 
--- tcp_server.py
+++ tcp_server.py
@@ -4,51 +4,55 @@ import time
 
 from motors import motor_xy
 
-# class TCPSocketHandler is used for getting packages across
+motor_table = motor_xy(12, 13, 50)
+motor_table.gotoposition(135,135)
+# class CommunicationHandler is used for getting packages across
 # and for running the motor applications 
-class TCPSocketHandler(socketserver.BaseRequestHandler):
-    # create a motorcontroller class
-    motor_table = motor_xy(12, 13, 50)
-    motor_table.gotoposition(135,135)
-#    for x in range(10):
-#        motor_table.set_speeds(100,100)
-#        time.sleep(0.1)
-#    for x in range(60):
-#        motor_table.set_speeds(-10,-50)
-#        time.sleep(0.1)
+class CommunicationHandler(socketserver.BaseRequestHandler):
     x, y = motor_table.get_positions()
 
     print("x_position=" +str(x) + " y_position=" +str(y))
     
     #handle a TCP-Package
     def handle(self):
-        print("TCPSocketHandler::handle()")
-        self.data = self.request.recv(1024).strip()     #get data
-        self.data = str(self.data, 'utf-8')
-        print("{} wrote:".format(self.client_address[0])+ self.data)
-        #converting data (which is byte) to string
-
+        print("CommunicationHandler::handle()")
+        self.data = self.request.recv(1024).strip() #get data
+        self.data = str(self.data, 'utf-8')         #convert data
         #get motorpositions for response
-        x,y = self.motor_table.get_positions()
- #       print(data)
-  #      self.request.sendall(self.data.upper())
+        x,y = motor_table.get_positions()
         #load data as json package
+        essential_load =["X", "Y", "pos"] 
         jdata = json.loads(self.data)
-        if 'Request' in jdata:      #if a request has been sent
+        error_str = ""
+        for i in essential_load:
+            if i not in jdata:
+                error_str += "Could not find essential " + i +" in this package! "
+
+        if error_str == "":
+            if jdata["pos"] == True:    #interpret if it is a coordinates package
+                motor_table.gotoposition(jdata["X"], jdata["Y"])  
+            else:                       #or a speed package
+                motor_table.set_speeds(jdata["X"], jdata["Y"])
+            self.data = "{\"X\"=" + str(x)+ "\"Y\":"+str(y)+"}"
+            self.request.sendall(self.data.encode('utf-8')) #return the current position
+        else:
+            print(error_str)
+            self.request.sendall(error_str.encode('utf8'))
+class RequestHandler(socketserver.BaseRequestHandler):
+    def handle(self):
+        print("RequestHandler::handle()")
+        self.data = self.request.recv(1024).strip() #geht data
+        self.data =str(self.data, 'utf-8')          #convert data
+        #get motorpositions for response
+        x,y = motor_table.get_positions()
+        jdata =json.loads(self.data)
+        if'Request' in jdata:
             if jdata["Request"] == "position": 
                 self.data = "{\"Answer\":position,\"x\"=" + str(x)+ "\"y\":"+str(y)+"}"
                 self.request.sendall(self.data.encode('utf-8'))     #give back an answer
             else:
-                self.request.sendall("Did not understand Request".encode('utf-8')) #give back an error message
-        if 'pos' in jdata:          #if a movement command has been sent
-            if jdata["pos"] == True:    #interpret if it is a coordinates package
-                self.motor_table.gotoposition(jdata["X"], jdata["Y"])  
-            else:                       #or a speed package
-                self.motor_table.set_speeds(jdata["X"], jdata["Y"])
-            self.data = "{\"X\"=" + str(x)+ "\"Y\":"+str(y)+"}"
-            self.request.sendall(self.data.encode('utf-8')) #return the current position
-
-
-
+                self.request.sendall("Did not understand Request".encode('utf-8')) #return an error message  
+        else:
+            self.request.sendall("Package does not include a Request".encode('utf-8'))  #return an error message          
 
     
